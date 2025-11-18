7f62b952cceff8578436ea1c558341390df7eab5
Danil Kazakov <dani4upa586@gmail.com>
2025-11-18 16:44:22 +0900

    chore: project init & first configuration

---
+++ Tally/Tally/TallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TallyApp.swift
+//  Tally
+//
+//  Created by Danil Kazakov on 18.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tally/Tally/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tally
+//
+//  Created by Danil Kazakov on 18.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
